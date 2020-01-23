1c75ba263bd78869f9deef60c94a74619ac7f7b8
Karl Horky <karl.horky@gmail.com>
2020-01-23 10:02:45 +0100

    Add parameter to links to show all messages (#3)
    thanks to @karlhorky

--- index.user.js
+++ index.user.js
@@ -6,6 +6,8 @@
 // @match https://github.com/notifications/beta?*
 // ==/UserScript==
 (function () {
+  const showAllMessagesParameter = 'show_full=true'
+
   function addClasses() {
     Array.from(document.querySelectorAll('.Label')).forEach(label => {
       const labelName = label.textContent.trim().replace(/\s+([a-z])/, (...args) => args[1].toUpperCase())
@@ -18,7 +20,19 @@
     })
   }
 
-  document.body.addEventListener('pjax:complete', addClasses)
+  function alwaysShowFull() {
+    Array.from(document.querySelectorAll('.notification-list-item-link')).forEach(anchor => {
+      if (anchor.search.includes(showAllMessagesParameter)) return
+      anchor.search += (anchor.search.startsWith('?') ? '&' : '?') + showAllMessagesParameter
+    })
+  }
 
-  addClasses()
+  function applyModifications() {
+    addClasses()
+    alwaysShowFull()
+  }
+
+  document.body.addEventListener('pjax:complete', applyModifications)
+
+  applyModifications()
 }())
